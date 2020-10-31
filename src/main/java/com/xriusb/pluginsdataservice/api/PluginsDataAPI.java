722cc83694dcfb035959f14065aee0a8d3fb6e47
xriusb <xaviriusb@hotmai.com>
2020-10-31 11:17:24 +0100

    adds compatibiity to return xml response format

--- src/main/java/com/xriusb/pluginsdataservice/api/PluginsDataAPI.java
+++ src/main/java/com/xriusb/pluginsdataservice/api/PluginsDataAPI.java
@@ -1,6 +1,8 @@
 package com.xriusb.pluginsdataservice.api;
 
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
 import com.xriusb.pluginsdataservice.model.HostResponse;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -9,14 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/dataService.com")
 public class PluginsDataAPI {
 
     @GetMapping(value="/getData", produces= MediaType.APPLICATION_XML_VALUE)
-    public ResponseEntity<HostResponse> getPluginsData(@RequestParam String accountCode,
-                                                       @RequestParam String targetDevice,
-                                                       @RequestParam String pluginVersion) {
-        return ResponseEntity.ok(HostResponse.builder().hostName("hola").pingTime(2).viewCode("adeu").build());
+    public ResponseEntity<String> getPluginsData(@RequestParam String accountCode,
+                                                 @RequestParam String targetDevice,
+                                                 @RequestParam String pluginVersion) throws IOException {
+
+        XmlMapper xmlMapper = new XmlMapper();
+        xmlMapper.configure(ToXmlGenerator.Feature.WRITE_XML_DECLARATION, true);
+        String xml = xmlMapper.writeValueAsString(HostResponse.builder().hostName("foo").pingTime(2).viewCode("baa").build());
+
+        return ResponseEntity.ok(xml);
     }
 }
--- src/main/java/com/xriusb/pluginsdataservice/model/HostResponse.java
+++ src/main/java/com/xriusb/pluginsdataservice/model/HostResponse.java
@@ -1,5 +1,7 @@
 package com.xriusb.pluginsdataservice.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,8 +9,12 @@ import lombok.Data;
 @Data
 @Builder
 @AllArgsConstructor
+@JacksonXmlRootElement(localName = "q")
 public class HostResponse {
+    @JsonProperty("h")
     private String hostName;
+    @JsonProperty("pt")
     private Integer pingTime;
+    @JsonProperty("c")
     private String viewCode;
 }
